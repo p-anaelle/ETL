39a8a59eab44bdf3f1dd60f410d59923e0dfbaab
-anaëlle- <anaelle.pinceloup@gmail.com>
2025-01-21 17:18:39 +0100

    ajout table orders

---
+++ dbt_quete/projet_dbt2/models/staging/stg_orders.sql
@@ -0,0 +1,12 @@
+with source as (
+    select * from {{ source('jaffle_shop', 'raw_orders') }}
+),
+renamed as (
+    select
+        id as order_id,
+        customer as customer_id
+        ordered_at as order_date
+        store_id
+    from source
+)
+select * from renamed
